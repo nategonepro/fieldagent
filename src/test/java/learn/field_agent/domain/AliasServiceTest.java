15306c354c9f4c303fdacd3012443727f51a3509
Nathanael Wisth <nategonepro@gmail.com>
2021-06-14 10:39:50 -0500

    service tests done

--- src/test/java/learn/field_agent/domain/AliasServiceTest.java
+++ src/test/java/learn/field_agent/domain/AliasServiceTest.java
@@ -1,12 +1,15 @@
 package learn.field_agent.domain;
 
 import learn.field_agent.data.AliasRepository;
+import learn.field_agent.models.Alias;
+import learn.field_agent.models.SecurityClearance;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class AliasServiceTest {
@@ -19,7 +22,72 @@ class AliasServiceTest {
 
     @Test
     void shouldAdd(){
+        Alias toAdd = new Alias(0,"Gerald","a straight up g(randpa)", 1);
+        when(aliasRepository.add(toAdd)).thenReturn(makeAlias());
+        Result<Alias> result = service.add(toAdd);
+        assertEquals(ResultType.SUCCESS, result.getType());
+        assertEquals(makeAlias(), result.getPayload());
+    }
 
+    @Test
+    void shouldNotAddWithSetId(){
+        Alias alias = makeAlias();
+        Result<Alias> result = service.add(alias);
+        assertFalse(result.isSuccess());
+    }
+
+    @Test
+    void shouldNotAddWithNullAlias(){
+        Result<Alias> result = service.add(null);
+        assertFalse(result.isSuccess());
+    }
+
+    @Test
+    void shouldNotAddWithMissingName(){
+        Alias alias = makeAlias();
+        alias.setName("");
+        Result<Alias> result = service.add(alias);
+        assertFalse(result.isSuccess());
+    }
+
+    @Test
+    void shouldUpdate(){
+        Alias toUpdate = new Alias(1, "My Boy", "Just a simple lad", 1);
+
+        when(aliasRepository.update(toUpdate)).thenReturn(true);
+        Result<Alias> result = service.update(toUpdate);
+        assertEquals(ResultType.SUCCESS, result.getType());
+    }
+
+    @Test
+    void shouldNotUpdateMissing(){
+        Alias toUpdate = makeAlias();
+        toUpdate.setAliasId(1500);
+
+        when(aliasRepository.update(toUpdate)).thenReturn(false);
+        Result<Alias> result = service.update(toUpdate);
+        assertEquals(ResultType.NOT_FOUND, result.getType());
+    }
+
+    @Test
+    void shouldNotUpdateWhenInvalid(){
+        Alias toUpdate = new Alias(1, null, null, 1);
+
+        Result<Alias> result = service.update(toUpdate);
+        assertEquals(ResultType.INVALID, result.getType());
+    }
+
+    @Test
+    void shouldNotUpdateWhenNull(){
+        Alias toUpdate = null;
+
+        Result<Alias> result = service.update(toUpdate);
+        assertEquals(ResultType.INVALID, result.getType());
+    }
+
+    Alias makeAlias(){
+        Alias alias = new Alias(1,"Gerald","a straight up g(randpa)",1);
+        return alias;
     }
 
 }
